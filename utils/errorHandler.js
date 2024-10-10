bc03926a0c6dc8d325d422838bb23e96ffab8049
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-10 18:21:00 -0300

    Aprimorando ErrorHandler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,19 @@
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json(
-      { message: 'Algo não saiu bem!', 
-        error: err.message }
-    );
-  };
-  
+
+export default class ErrorHandler extends Error {
+
+    constructor(statusCode, message) {
+        super()
+        this.statusCode = statusCode;
+        this.message = message;
+    
+        function handler(err, res) {
+            console.error(err.stack);
+            const { statusCode, message } = err;
+            return res.status(statusCode || 500).json({ 
+                status: "error",
+                statusCode: statusCode || 500,
+                message: message || "Erro interno no Servidor"
+            });
+        }
+    }
+}
