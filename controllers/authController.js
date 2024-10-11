dd889bff281a2912c6dcd9b8194cb95a0b0a11b3
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-11 00:16:10 -0300

    Adicionando verifyToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,13 +3,13 @@ import ErrorHandler from '../utils/errorHandler.js';
 
 export default class AuthController {
 
-    async loginAccount(req, res) {
+    async loginAccount(req, res, next) {
         try {
-            const AuthService = new AuthService();
-            const authenticateUser = await AuthService.authenticateUser(req.body);
+            const authService = new AuthService();
+            const authenticateUser = await authService.authenticateUser(req.body);
             if (authenticateUser === null) {
                 return next(new ErrorHandler(404, "O e-mail informado não está cadastrado."));
-            }                
+            }
             if (authenticateUser === true) {
                 return next(new ErrorHandler(403, "Não foi possível encontrar um processo para esse usuário."));
             }
@@ -18,17 +18,28 @@ export default class AuthController {
             }
             return res.status(200).json({ message: 'Login aprovado', token: authenticateUser });
         } catch (error) {
-            return next(new ErrorHandler(500, 'Erro ao tentar realizar o login'));
+            // return next(new ErrorHandler(500, 'Erro ao tentar realizar o login'));
+            return next(new ErrorHandler(500, error));
         }
     }
 
     async createAccount(req, res, next) {
         try {
             const authService = new AuthService();
-            const createUser = await authService.createUser(req.body)
+            const createUser = await authService.createUser(req.body);
             return res.status(201).json(createUser);
         } catch (error) {
-            return next(new ErrorHandler(500, `Erro ao se conectar: ${error}`));
+            return next(new ErrorHandler(500, error));
+        }
+    }
+
+    async verifyToken(req, res, next) {
+        try {
+            const authService = new AuthService();
+            const verifyAuthentication = await authService.verifyAuthentication(req.body);
+            return res.status(200).json(verifyAuthentication)
+        } catch (error) {
+            return next(new ErrorHandler(500, error));
         }
     }
 
