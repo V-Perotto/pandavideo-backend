d39d170fbc0915827aedd1d5450fe06613efeee7
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-11 00:16:23 -0300

    Adicionando rota para verifyToken

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ import AuthController from '../controllers/authController.js';
 const router = Router();
 
 router.post('/signup', (req, res, next) => new AuthController().createAccount(req, res, next));
-router.post('/login', (req, res) => new AuthController().loginAccount(req, res));
+router.post('/login', (req, res, next) => new AuthController().loginAccount(req, res, next));
+router.post('/auth', (req, res, next) => new AuthController().verifyToken(req, res, next));
 
 export default router;
