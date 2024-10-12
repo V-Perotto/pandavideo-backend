ba987a811e6dd8bb7ce76d54a69eb688756daec8
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-12 01:18:56 -0300

    Corrigindo coleta de body

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,7 @@ export default class VideoController {
 
     async getVideos(req, res, next) {
         try {
-            const videos = await VideoService.getVideos();
+            const videos = await VideoService.getVideos(req.body);
             res.status(200).json(videos);
         } catch (error) {
             return next(new ErrorHandler(500, "Erro ao buscar o(s) vídeo(s)."));
