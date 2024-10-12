0b2963173ad1912fa6fefe3124b166c34c4a0c81
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-11 22:53:10 -0300

    Corrigindo ErrorHandler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -2,18 +2,18 @@
 export default class ErrorHandler extends Error {
 
     constructor(statusCode, message) {
-        super()
+        super(message);
         this.statusCode = statusCode;
         this.message = message;
-    
-        function handler(err, res) {
-            console.error(err.stack);
-            const { statusCode, message } = err;
-            return res.status(statusCode || 500).json({ 
-                status: "error",
-                statusCode: statusCode || 500,
-                message: message || "Erro interno no Servidor"
-            });
-        }
+    }
+
+    static handler(err, res) {
+        console.error(err.stack);
+        const { statusCode, message } = err;
+        return res.status(statusCode || 500).json({ 
+            status: "error",
+            statusCode: statusCode || 500,
+            message: message || "Erro interno no Servidor"
+        });
     }
 }
