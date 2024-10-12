be2feb06c7b9bb41eefb880a8b1d022fd9242e86
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-11 22:50:52 -0300

    Aprimorando AuthController

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,28 +8,39 @@ export default class AuthController {
             const authService = new AuthService();
             const authenticateUser = await authService.authenticateUser(req.body);
             if (authenticateUser === null) {
-                return next(new ErrorHandler(404, "O e-mail informado não está cadastrado."));
+                return next(new ErrorHandler(
+                    404, "O e-mail informado não está cadastrado."
+                ));
             }
             if (authenticateUser === true) {
-                return next(new ErrorHandler(403, "Não foi possível encontrar um processo para esse usuário."));
+                return next(new ErrorHandler(
+                    403, "Não foi possível encontrar um processo para esse usuário."
+                ));
             }
             if (authenticateUser === false) {
-                return next(new ErrorHandler(401, "A senha informada está inválida."));
+                return next(new ErrorHandler(
+                    401, "A senha informada está inválida."
+                ));
             }
             return res.status(200).json({ message: 'Login aprovado', token: authenticateUser });
         } catch (error) {
-            // return next(new ErrorHandler(500, 'Erro ao tentar realizar o login'));
-            return next(new ErrorHandler(500, error));
+            return next(new ErrorHandler(
+                500, 'Erro ao tentar realizar o login'
+            ));
         }
     }
 
     async createAccount(req, res, next) {
         try {
+            const { password } = req.body;
             const authService = new AuthService();
-            const createUser = await authService.createUser(req.body);
-            return res.status(201).json(createUser);
+            await authService.createUser(req.body);
+            const authenticateUser = await authService.authenticateUser(
+                { email: req.body.email, password: password }
+            );
+            return res.status(201).json(authenticateUser);
         } catch (error) {
-            return next(new ErrorHandler(500, error));
+            return next(new ErrorHandler(500, error.message));
         }
     }
 
@@ -39,7 +50,17 @@ export default class AuthController {
             const verifyAuthentication = await authService.verifyAuthentication(req.body);
             return res.status(200).json(verifyAuthentication)
         } catch (error) {
-            return next(new ErrorHandler(500, error));
+            return next(new ErrorHandler(500, error.message));
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            const authService = new AuthService();
+            const users = await authService.getUsers(req.body);
+            return res.status(200).json(users)
+        } catch (error) {
+            return next(new ErrorHandler(500, error.message));
         }
     }
 
