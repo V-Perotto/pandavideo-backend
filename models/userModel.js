555c4adea44868c44d39f44d85211959c4e4d43a
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-12 01:12:50 -0300

    Correção no UserModel

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,8 @@ import { mongoose } from "mongoose";
 
 const userModel = new mongoose.Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  token: { type: String, required: false },
 }, { timestamps: {} });
 
 export default mongoose.model('User', userModel);
