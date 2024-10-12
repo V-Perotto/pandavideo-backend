ac29b3824f2a5270b4a6cdccfcbb5cdfe4b5f8d0
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-11 22:51:01 -0300

    Adicionando rota /users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/signup', (req, res, next) => new AuthController().createAccount(req, res, next));
 router.post('/login', (req, res, next) => new AuthController().loginAccount(req, res, next));
 router.post('/auth', (req, res, next) => new AuthController().verifyToken(req, res, next));
+router.get('/users', (req, res, next) => new AuthController().getUsers(req, res, next));
 
 export default router;
