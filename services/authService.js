ddb61bfee92549c8cbf513c2c21039c585f578c2
Vittorio Perotto <vittorio.perotto@outlook.com>
2024-10-13 17:04:40 -0300

    Aprimorando authService

--- services/authService.js
+++ services/authService.js
@@ -10,39 +10,48 @@ export default class AuthService {
             email: data.email
         });
         if (userExists.length > 0) {
-            throw Error("Esse usuário já existe.");
+            return { 
+                status: false, 
+                message: "Esse usuário já existe." 
+            };
         }
         if (data.password.length < 8) {
-            throw Error("O tamanho da senha é inválido, insira outra senha.");
+            return { 
+                status: false, 
+                message: "O tamanho da senha é inválido, insira outra senha." 
+            };
         }
         data.password = await bcrypt.hash(data.password, 10);
         data.token = "";
-        await userModel.create(data);
-        return true;
+        const userCreated = await userModel.create(data);
+        return { status: true, message: userCreated };
     }
 
     async authenticateUser(data) {
         const authenticateUser = await userModel.findOne({ 
             email: data.email 
         });
+        if (!authenticateUser) {
+            return { status: null, message: "O e-mail informado não está cadastrado." };
+        }
         const checkPassword = await bcrypt.compare(
             data.password, 
             authenticateUser.password
         );
         if (!checkPassword) {
-            return false;
+            return { status: false, message: "A senha informada está inválida."};
         }
         const token = jwt.sign(
             { id: authenticateUser._id }, 
             process.env.JWT_SECRET, 
-            { expiresIn: '1h' }
+            { expiresIn: '24h' }
         );
-        const teste = await userModel.findOneAndUpdate(
+        await userModel.findOneAndUpdate(
             { _id: authenticateUser._id }, 
             { $set: { token: token } },
             { new: true}
         );
-        return token;
+        return { status: true, message: token };
     }
 
     async getUsers() {
